14bf7dc83d13f0f141e0b91567eefcc52f2de337
Saadmrp1038 <saadmrp222@gmail.com>
2024-07-08 00:30:43 +0600

    updated limit

--- app/api/api_v1/endpoints/query.py
+++ app/api/api_v1/endpoints/query.py
@@ -24,7 +24,7 @@ def create_chat_completion(query, search_results):
 
     prompt += "\nPlease provide a detailed and humanized response to the query based on the search results above."
     response = openaiClient.chat.completions.create(
-        model="gpt-3.5-turbo-0125",
+        model="gpt-4o",
         messages=[
             {"role": "system", "content": "You are a helpful assistant."},
             {"role": "user", "content": prompt}
